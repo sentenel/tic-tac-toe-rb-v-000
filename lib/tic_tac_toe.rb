8380f3877883e63a636e2052973760abf0978262
Seth Wax <sethlwax@gmail.com>
2018-02-22 11:10:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,3 +19,7 @@ end
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
